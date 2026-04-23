1ceb724a3b0fab0ff61d1ca201a7bbb7697c682b
Abdul Ahad Javaid <abdulahad@192.168.10.7>
2026-04-23 15:11:13 +0500

    implement security filter chain for User

--- src/main/java/com/jobtracker/job_tracker/security/SecurityConfig.java
+++ src/main/java/com/jobtracker/job_tracker/security/SecurityConfig.java
@@ -1,15 +1,36 @@
 package com.jobtracker.job_tracker.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private CustomUserDetailService userDetailService;
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // disable crsf
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.
+                        requestMatchers(HttpMethod.POST,"/api/users/register").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/users").permitAll()
+                        .anyRequest().authenticated()).httpBasic(basic ->
+                        basic.realmName("Job Tracker"));
+        return http.build();
     }
+
+
 }
