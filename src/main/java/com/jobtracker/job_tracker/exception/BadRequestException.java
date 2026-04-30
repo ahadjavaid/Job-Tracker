0aa136fad2655ddfe122a35a8daa1badcd9a2fa0
Abdul Ahad Javaid <abdulahad@192.168.10.13>
2026-04-30 15:53:07 +0500

    add exception handling functionality

---
+++ src/main/java/com/jobtracker/job_tracker/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.jobtracker.job_tracker.exception;
+
+public class BadRequestException extends RuntimeException{
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/jobtracker/job_tracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,85 @@
+package com.jobtracker.job_tracker.exception;
+
+import com.jobtracker.job_tracker.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // handle 404 - resource not found
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex) {
+
+        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(), LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    //handle 409 - Duplicate resource
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateResource(DuplicateResourceException ex) {
+
+        ErrorResponse error = new ErrorResponse(HttpStatus.CONFLICT.value(),
+                ex.getMessage(), LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    //handle 400 - bad request
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequest(BadRequestException ex) {
+
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(), LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    // handle 401 - unauthorized
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorized(UnauthorizedException ex){
+
+        ErrorResponse error = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(),
+                ex.getMessage(), LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    }
+
+    // handle validation errors
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex){
+
+        String errors = ex.getBindingResult().getFieldErrors()
+                .stream().map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(),
+                errors,LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    // handle any other un-expected error
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleOtherException(Exception ex) {
+
+        ErrorResponse error = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(), LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+}
